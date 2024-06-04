d814ce8c7185a7832e5190c24e7da24f6cc9e755
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2024-06-04 19:40:42 +0700

    Fixed (token JWT)

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,32 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const { jwtSecret } = require('../config/jwtConfig');
-// const { isTokenBlacklisted } = require('./blacklistToken');
-
-// async function authenticate(req, res, next) {
-//   // const token = req.headers['authorization'];
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader && authHeader.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(401).json({ status: 'fail', message: 'Unauthorized', data: {} });
-//   }
-
-//   const jwtToken = token.split(' ')[1];
-
-//   const isBlacklisted = await isTokenBlacklisted(jwtToken);
-//   if (isBlacklisted) {
-//     return res.status(401).json({ status: 'fail', message: 'Token has been revoked', data: {} });
-//   }
-
-//   jwt.verify(jwtToken, jwtSecret, (err, decoded) => {
-//     if (err) {
-//       return res.status(403).json({ status: 'fail', message: 'Forbidden', data: {} });
-//     }
-//     req.user = decoded;
-//     next();
-//   });
-// }
-
 const jwt = require('jsonwebtoken');
 const { secret } = require('../config/jwtConfig');
 const { isTokenBlacklisted } = require('./blacklistToken');
@@ -43,6 +14,13 @@ function authenticate(req, res, next) {
     });
   }
 
+  if (isTokenBlacklisted(token)) {
+    return res.status(401).json({
+      status: 'fail',
+      message: 'Token has been revoked',
+      data: {}
+    });
+  }
 
   jwt.verify(token, secret, (err, user) => {
     if (err) {
@@ -56,12 +34,5 @@ function authenticate(req, res, next) {
     next();
   });
 
-  if (isTokenBlacklisted(token)) {
-    return res.status(401).json({
-      status: 'fail',
-      message: 'Token has been revoked',
-      data: {}
-    });
-  }
 }
 module.exports = authenticate;
