bd138a3c632dbf68e7274519b5e725d709a41732
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2024-06-13 03:55:57 +0700

    fix (date)

--- src/controllers/predictionController.js
+++ src/controllers/predictionController.js
@@ -1,6 +1,7 @@
 const { predict } = require('../services/inferenceServices')
 const InputError = require('../exceptions/InputError');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
+const moment = require('moment-timezone');
 const crypto = require('crypto');
 const { Result } = require('express-validator');
 const {storeData, getPredictionByDate} = require('../services/storeData');
@@ -74,14 +75,17 @@ async function savePredictHandler(req, res, next) {
         const inputData = req.body;
         // const { result } = inputData;
         const email = req.user.email;
-        const today = new Date().toISOString().split('T')[0];
+        // const today = new Date().toISOString().split('T')[0];
+        // const today = moment().tz('Asia/Jakarta').format('YYYY-MM-DD')
+        const today = moment().tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss');
         const existingPrediction = await getPredictionByDate(email, today);
         console.log(inputData,"Pass test 1 !!");
         const dataToSave = {
             ...inputData,
             email,
-            created_at: existingPrediction ? existingPrediction.data().created_at : new Date().toISOString(),
-            update_at: new Date().toISOString()
+            created_at: existingPrediction ? existingPrediction.data().created_at : today,
+            // update_at: moment().tz('Asia/Jakarta').toISOString()
+            update_at : today
         };
         console.log(dataToSave, "Pass Test 2 !!");
         if (existingPrediction) {
--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,4 +1,5 @@
 const { Firestore } = require('@google-cloud/firestore');
+const moment = require('moment-timezone')
 
 async function storeData(id, data) {
   const db = new Firestore();
@@ -11,17 +12,34 @@ async function storeData(id, data) {
 async function getPredictionByDate(email, date){
   const db = new Firestore();
   const predictCollection = db.collection('predictions');
+  // const startOfDay = new Date(date);
+  // startOfDay.setHours(0, 0, 0, 0);
+  // const endOfDay = new Date(date);
+  // endOfDay.setHours(23, 59, 59, 999);
+  // const startOfDay = moment(date).tz('Asia/Jakarta', true).startOf('day').toDate();
+  // const endOfDay = moment(date).tz('Asia/Jakarta', true).endOf('day').toDate();
+  // date = new Date();
+  date = moment().tz('Asia/Jakarta');
+  const startOfDay = date.clone().startOf('day').tz('Asia/Jakarta', true).toDate();
+  const endOfDay = date.clone().endOf('day').tz('Asia/Jakarta', true).toDate();
+
+  const jakartaTime = moment().tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss');
+  console.log(jakartaTime)
+  // tanggal = date.getDate();
+  // console.log(tanggal)
+  console.log(startOfDay)
+  console.log(endOfDay)
   const snapshot = await predictCollection
     .where('email', '==', email)
-    .where('created_at', '>=', date+'T00:00:00.000Z')
-    .where('created_at', '<=', date+'T23:59:59.999Z')
-    .get()
+    // .where('created_at', '>=', startOfDay.toISOString())
+    .where('created_at', '==', jakartaTime)
+    .get();
 
   if (snapshot.empty){
-    return null
+    return null;
   }
 
   return snapshot.docs[0];
-
 }
+
 module.exports = {storeData, getPredictionByDate};
